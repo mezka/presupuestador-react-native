8c25eb2ba86b7e53a47a13ba5c3eec29b671b163
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-10-01 19:41:34 -0300

    Add goBack method to RootNavigation component

--- src/components/RootNavigation.ts
+++ src/components/RootNavigation.ts
@@ -5,3 +5,7 @@ export const navigationRef = createRef();
 export function navigate(name, params) {
     navigationRef.current?.navigate(name, params);
 }
+
+export function goBack(){
+    navigationRef.current?.goBack();
+}

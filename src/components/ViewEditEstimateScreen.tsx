859dfc4e641a0a389da1fc84b1966697b046266f
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-10-05 16:00:37 -0300

    Fix Button inside DataTable.Cell error

--- src/components/ViewEditEstimateScreen.tsx
+++ src/components/ViewEditEstimateScreen.tsx
@@ -2,14 +2,13 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getEstimates, exportEstimate } from '../actions/estimates';
 import { View, StyleSheet } from 'react-native';
-import { DataTable, Button } from 'react-native-paper';
+import { DataTable, Text, Button } from 'react-native-paper';
 import * as RootNavigation from './RootNavigation';
 
 const ViewEditEstimateScreen = (props) => {
 
   const dispatch = useDispatch();
   const estimates = useSelector(state => state.estimates.estimates);
-  const userId = useSelector(state => state.auth.user.id);
 
   useEffect(() => {
     dispatch(getEstimates());
@@ -22,36 +21,33 @@ const ViewEditEstimateScreen = (props) => {
     dispatch(exportEstimate(estimateId, 'pdf'));
   };
 
+  const estimateDataTableCells = estimates.map((estimate) => {
+      let estimateitems_total = estimate.estimateitems.reduce((acc, curr) => {return acc+(curr.unitprice*curr.quantity)}, 0);
+      let total_label = `$${estimateitems_total}`;
+      return (
+        <DataTable.Row style={{paddingLeft: 0, paddingRight: 0}} key={estimate.id}>
+          <DataTable.Cell style={{backgroundColor: 'red'}} >{estimate.id}</DataTable.Cell>
+          <DataTable.Cell>{estimate.createdAt.substring(0, 10)}</DataTable.Cell>
+          <DataTable.Cell>{estimate.client.name}</DataTable.Cell>
+          <DataTable.Cell>{total_label}</DataTable.Cell>
+          <DataTable.Cell onPress={() => handleEditEstimatePress(estimate)}>X</DataTable.Cell>
+          <DataTable.Cell style={{backgroundColor: 'red'}} onPress={() => handleDownloadAsPDFPress(estimate.id)}>X</DataTable.Cell>
+        </DataTable.Row>
+      );
+  });
+
   return  <View style={ styles.parentView }>
-            <DataTable>
-              <DataTable.Header>
-                <DataTable.Title sortDirection='ascending'>Presupuesto #</DataTable.Title>
+            <DataTable style= {styles.dataTable}>
+              <DataTable.Header style={{backgroundColor: 'blue', paddingLeft: 0, paddingRight: 0}}>
+                <DataTable.Title style={{backgroundColor: 'red'}}>N°</DataTable.Title>
                 <DataTable.Title>Fecha</DataTable.Title>
                 <DataTable.Title>Cliente</DataTable.Title>
                 <DataTable.Title>Total</DataTable.Title>
-                <DataTable.Title>Validez</DataTable.Title>
-                <DataTable.Title></DataTable.Title>
-                <DataTable.Title></DataTable.Title>
+                <DataTable.Title>Editar</DataTable.Title>
+                <DataTable.Title style={{backgroundColor: 'red'}}>PDF</DataTable.Title>
               </DataTable.Header>
-              { estimates.length ?
-                  estimates.map((estimate) => {
-                    if (estimate.user.id === userId) {
-                      let estimateitems_total = estimate.estimateitems.reduce((acc, curr) => {return acc+(curr.unitprice*curr.quantity)}, 0);
-                      let total_label = `$${estimateitems_total}`;
-                      return (
-                        <DataTable.Row key={estimate.id}>
-                          <DataTable.Cell>{estimate.id}</DataTable.Cell>
-                          <DataTable.Cell>{estimate.createdAt.substring(0, 10)}</DataTable.Cell>
-                          <DataTable.Cell>{estimate.client.name}</DataTable.Cell>
-                          <DataTable.Cell>{total_label}</DataTable.Cell>
-                          <DataTable.Cell>{estimate.validFor + ' meses'}</DataTable.Cell>
-                          <DataTable.Cell><Button mode="contained" onPress={() => handleEditEstimatePress(estimate)}>Editar</Button></DataTable.Cell>
-                          <DataTable.Cell><Button mode="contained" onPress={() => handleDownloadAsPDFPress(estimate.id)}>PDF</Button></DataTable.Cell>
-                        </DataTable.Row>
-                      )
-                    }
-                  })
-                : <DataTable.Row></DataTable.Row>
+              { 
+                estimateDataTableCells
               }
             </DataTable>
           </View>
@@ -61,8 +57,10 @@ const styles = StyleSheet.create({
   parentView: {
     alignItems: 'center',
     marginTop: 120,
-    marginHorizontal: 100,
     height: 200
+  },
+  dataTable: {
+    backgroundColor: 'green',
   }
 })
 

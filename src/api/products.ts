688d294d9b8cb66662f3de691643fba7690ed04a
Matias Napoli <mnap@pop-os.localdomain>
2020-10-08 15:31:03 -0300

    Put omitted 'return'

--- src/api/products.ts
+++ src/api/products.ts
@@ -4,5 +4,5 @@ import Constants from 'expo-constants';
 const api_url = Constants.manifest.extra.api_url;
 
 export const getProducts = (token) => {
-  ky.get(`${ api_url }/products`, { headers: { Authorization: token } }).json();
+  return ky.get(`${ api_url }/products`, { headers: { Authorization: token } }).json();
 }

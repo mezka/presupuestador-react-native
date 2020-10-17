0d76fb5610def4a366b18dceb12cfb8ce4280b59
Matias Napoli <mnap@pop-os.localdomain>
2020-10-17 13:57:21 -0300

    Login TextInput validations

--- src/components/AuthenticationScreen.tsx
+++ src/components/AuthenticationScreen.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../actions/auth';
 import { View, StyleSheet, Text } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { Button, HelperText, TextInput } from 'react-native-paper';
 import * as RootNavigation from './RootNavigation';
 
 const AuthenticationScreen = props => {
@@ -17,20 +17,36 @@ const AuthenticationScreen = props => {
     setUser({...user, password: passwordText});
   };
 
+  const inputEmailHasErrors = () => {
+    return !(/^$|^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(user.email));
+  };
+  
+  const inputPasswordHasErrors = () => {
+    return !(/^$|^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/g.test(user.password));
+  };
+  
   const handleLoginPress = () => {
-    dispatch(loginUser(user));
+    if (!(user.email==='' || user.password==='' || inputEmailHasErrors() || inputPasswordHasErrors())) {
+      dispatch(loginUser(user));
+    }
   };
   
   const navigateToSignup = () => {
     RootNavigation.navigate('Signup', {});
   };
-
+  
   return (
     <View style={styles.parentView}>
       <View style={styles.loginView}>
         <View style={styles.inputView}>
           <TextInput label="E-mail" placeholder="email@example.com" value={user.email} onChangeText={emailTextChange} />
+          <HelperText type="error" visible={inputEmailHasErrors()}>
+            Email inválido
+          </HelperText>
           <TextInput label="Contraseña" value={user.password} onChangeText={passwordTextChange} />
+          <HelperText type="error" visible={inputPasswordHasErrors()}>
+            Contraseña inválida (8 caracteres, mínimo un número)
+          </HelperText>
         </View>
         <View style={styles.buttonView}>
           <Button mode="contained" onPress={handleLoginPress}>Ingresá</Button>

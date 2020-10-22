154059ad893e060b8b467c1e1ea3adb9b36f1183
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-10-22 17:37:51 -0300

    Fix build config, change icon

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ const ip = require('ip');
 const env_vars = require('dotenv').config();
 
 export default {
-  name: "react-native-presupuestador-typescript",
-  slug: "react-native-presupuestador-typescript",
+  name: "Mesquita Hnos - Presupuestador",
+  slug: "presupuestador-mesquita-hnos",
   version: "0.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -20,6 +20,9 @@ export default {
   ],
   ios: {
     supportsTablet: true
+  },
+  android: {
+    "package": "com.mesquita.presupuestador"
   },
   extra: {
     api_url: env_vars.API_URL || `http://${ip.address()}:3030` //used to set up client api urls

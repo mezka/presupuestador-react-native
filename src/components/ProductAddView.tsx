7249c85d4342740055cbb2db4c51daa5574ce969
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2020-12-09 01:59:14 -0300

    Optimize ProductAddView with useMemo

--- src/components/ProductAddView.tsx
+++ src/components/ProductAddView.tsx
@@ -1,25 +1,25 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useMemo } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Searchbar, Text, Checkbox, Button, RadioButton, Chip } from 'react-native-paper';
 
 const productCheckboxReducer = (state, action) => {
-  switch(action.type){
+  switch (action.type) {
     case 'toggle':
-      const newCheckboxState = {...state.checkboxes, [action.productid]: !state.checkboxes[action.productid]};
-      return {...state, checkboxes: {...newCheckboxState}, products: Object.entries(newCheckboxState).filter(entries => entries[1]).map(entries => Number(entries[0]))};
+      const newCheckboxState = { ...state.checkboxes, [action.productid]: !state.checkboxes[action.productid] };
+      return { ...state, checkboxes: { ...newCheckboxState }, products: Object.entries(newCheckboxState).filter(entries => entries[1]).map(entries => Number(entries[0])) };
     default:
-      return {...state};
+      return { ...state };
   }
 };
 
 const categoriesReducer = (state, action) => {
 
-  switch(action.type){
+  switch (action.type) {
     case 'toggle':
-      const newChipState = {...state.chips, [action.categoryid]: !state.chips[action.categoryid]};
-      return {...state, chips: {...newChipState}, categories: Object.entries(newChipState).filter(entries => entries[1]).map(entries => Number(entries[0]))};
+      const newChipState = { ...state.chips, [action.categoryid]: !state.chips[action.categoryid] };
+      return { ...state, chips: { ...newChipState }, categories: Object.entries(newChipState).filter(entries => entries[1]).map(entries => Number(entries[0])) };
     default:
-      return {...state};
+      return { ...state };
   }
 }
 
@@ -28,15 +28,15 @@ const createChipsReducer = (chips, currentCategory) => {
   return chips;
 }
 
-const ProductAddView = ({categories, filteredProducts, addEstimateItem, setProductsCategoriesFilter, setProductsSearchAndCategoriesFilter}) => {
+const ProductAddView = ({ categories, filteredProducts, addEstimateItem, setProductsCategoriesFilter, setProductsSearchAndCategoriesFilter }) => {
 
   const [searchQuery, setSearchQuery] = useState('');
-  const [checkboxes, dispatchCheckbox] = useReducer(productCheckboxReducer, {checkboxes: {}, products: []});
-  const [chips, dispatchChip] = useReducer(categoriesReducer, {chips: categories.reduce(createChipsReducer, {}), categories: categories.map(category => category.id)});
+  const [checkboxes, dispatchCheckbox] = useReducer(productCheckboxReducer, { checkboxes: {}, products: [] });
+  const [chips, dispatchChip] = useReducer(categoriesReducer, { chips: categories.reduce(createChipsReducer, {}), categories: categories.map(category => category.id) });
 
 
   useEffect(() => {
-    if(searchQuery){
+    if (searchQuery) {
       setProductsSearchAndCategoriesFilter(searchQuery, chips.categories);
     } else {
       setProductsCategoriesFilter(chips.categories);
@@ -53,27 +53,22 @@ const ProductAddView = ({categories, filteredProducts, addEstimateItem, setProdu
     });
   };
 
-  const productList = filteredProducts.map(product => {
+  const productList = useMemo(() => (filteredProducts.map(product => (
+    <View key={product.id} style={styles.productList}>
+      <Checkbox status={checkboxes.checkboxes[product.id] ? 'checked' : 'unchecked'} onPress={() => dispatchCheckbox({ type: 'toggle', productid: product.id })} />
+      <Text>{product.modelname}</Text>
+    </View>
+  ))), [filteredProducts, checkboxes])
 
-    return(
-      <View key={product.id} style={styles.productList}>
-        <Checkbox status={checkboxes.checkboxes[product.id]? 'checked' : 'unchecked'} onPress={() => dispatchCheckbox({type: 'toggle', productid: product.id})}/>
-        <Text>{product.modelname}</Text>
-      </View>
-    );
-  });
-
-  const categoriesList = categories.map(category => {
-    return(
-      <View key={category.id} style={styles.category}>
-        <Chip selected={chips.chips[category.id]} onPress={() => dispatchChip({type: 'toggle', categoryid: category.id})}>{category.name}</Chip>
-      </View>
-    );
-  });
+  const categoriesList = useMemo(() => (categories.map(category => (
+    <View key={category.id} style={styles.category}>
+      <Chip selected={chips.chips[category.id]} onPress={() => dispatchChip({ type: 'toggle', categoryid: category.id })}>{category.name}</Chip>
+    </View>
+  ))), [categories, chips])
 
   return (
     <View>
-      <Searchbar placeholder="Buscar" onChangeText={onChangeSearch} value={searchQuery}/>
+      <Searchbar placeholder="Buscar" onChangeText={onChangeSearch} value={searchQuery} />
       <RadioButton.Group>
         <View style={styles.categoriesList}>
           {categoriesList}

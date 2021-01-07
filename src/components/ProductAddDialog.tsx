7de5ee4871009eb3508d3addc0e42a90ddac9c9f
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2021-01-06 23:43:37 -0300

    Fix add checked product when changing category

--- src/components/ProductAddDialog.tsx
+++ src/components/ProductAddDialog.tsx
@@ -2,23 +2,16 @@ import React, { useState, useEffect, useReducer, useMemo } from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import { Searchbar, Checkbox, Button, Dialog, Chip, withTheme} from 'react-native-paper';
 
-const productCheckboxReducer = (state, action) => {
-  switch (action.type) {
-    case 'toggle':
+
+const productAndCategoriesReducer = (state, action) => {
+  switch(action.type) {
+    case 'TOGGLE_PRODUCT':
       const newCheckboxState = { ...state.checkboxes, [action.productid]: !state.checkboxes[action.productid] };
       return { ...state, checkboxes: { ...newCheckboxState }, products: Object.entries(newCheckboxState).filter(entries => entries[1]).map(entries => Number(entries[0])) };
-    default:
-      return { ...state };
-  }
-};
-
-const categoriesReducer = (state, action) => {
-
-  switch (action.type) {
-    case 'toggle':
+    case 'TOGGLE_CATEGORY':
       const newChipState = { ...state.chips, [action.categoryid]: !state.chips[action.categoryid] };
-      return { ...state, chips: { ...newChipState }, categories: Object.entries(newChipState).filter(entries => entries[1]).map(entries => Number(entries[0])) };
-    default:
+      return { ...state, chips: { ...newChipState }, categories: Object.entries(newChipState).filter(entries => entries[1]).map(entries => Number(entries[0])), checkboxes: {}, products: []}
+    default: 
       return { ...state };
   }
 }
@@ -31,39 +24,38 @@ const createChipsReducer = (chips, currentCategory) => {
 const ProductAddDialog = ({ categories, filteredProducts, addEstimateItem, setProductsCategoriesFilter, setProductsSearchAndCategoriesFilter, visible, toggleDialog }) => {
 
   const [searchQuery, setSearchQuery] = useState('');
-  const [checkboxes, dispatchCheckbox] = useReducer(productCheckboxReducer, { checkboxes: {}, products: [] });
-  const [chips, dispatchChip] = useReducer(categoriesReducer, { chips: categories.reduce(createChipsReducer, {}), categories: categories.map(category => category.id) });
-
+  const [state, dispatch] = useReducer(productAndCategoriesReducer, { checkboxes: {}, products: [], chips: categories.reduce(createChipsReducer, {}), categories: categories.map(category => category.id)});
 
   useEffect(() => {
     if (searchQuery) {
-      setProductsSearchAndCategoriesFilter(searchQuery, chips.categories);
+      setProductsSearchAndCategoriesFilter(searchQuery, state.categories);
     } else {
-      setProductsCategoriesFilter(chips.categories);
+      setProductsCategoriesFilter(state.categories);
     }
-  }, [searchQuery, chips.categories]);
+  }, [searchQuery, state.categories]);
 
   const onChangeSearch = (queryString: string) => {
     setSearchQuery(queryString);
   };
 
   const addEstimateItems = () => {
-    checkboxes.products.forEach((product: number) => {
+    state.products.forEach((product: number) => {
       addEstimateItem(product);
     });
     toggleDialog();
   };
+
   const productList = useMemo(() => (filteredProducts.map(product => (
     <View key={product.id} style={styles.productList}>
-      <Checkbox.Item style={styles.checkbox} labelStyle={{}} status={checkboxes.checkboxes[product.id] ? 'checked' : 'unchecked'} onPress={() => dispatchCheckbox({ type: 'toggle', productid: product.id })} label={product.modelname}/>
+      <Checkbox.Item style={styles.checkbox} labelStyle={{}} status={state.checkboxes[product.id] ? 'checked' : 'unchecked'} onPress={() => dispatch({ type: 'TOGGLE_PRODUCT', productid: product.id })} label={product.modelname}/>
     </View>
-  ))), [filteredProducts, checkboxes])
+  ))), [filteredProducts, state.checkboxes])
 
   const categoriesList = useMemo(() => (categories.map(category => (
     <View key={category.id} style={styles.category}>
-      <Chip selected={chips.chips[category.id]} onPress={() => dispatchChip({ type: 'toggle', categoryid: category.id })}>{category.name}</Chip>
+      <Chip selected={state.chips[category.id]} onPress={() => dispatch({ type: 'TOGGLE_CATEGORY', categoryid: category.id })}>{category.name}</Chip>
     </View>
-  ))), [categories, chips])
+  ))), [state.categories, state.chips])
 
   return (
     <Dialog visible={visible} onDismiss={toggleDialog} style={styles.dialog}>
